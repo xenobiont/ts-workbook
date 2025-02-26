d72d65e7ee4ffde4052f57b3bfd6ee1b68d85555
xenobiont <larinsson@gmail.com>
2025-02-26 16:59:12 +0200

    delete solution

--- src/Person.ts
+++
@@ -1,7 +0,0 @@
-export class Person {
-	constructor(public name: string) {}
-
-	getInfo() {
-		return `person: ${this.name}`;
-	}
-}
--- src/Position.ts
+++
@@ -1,5 +0,0 @@
-export enum Position {
-  MANAGER,
-  DEVELOPER,
-  DIRECTOR,
-}
